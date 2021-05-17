af6a756792d5d31301b48097e961c6f612d98057
doubaniux <goodsir@vivaldi.net>
2021-05-17 15:53:08 +0200

    add slogan

--- src/components/SexChoice.js
+++ src/components/SexChoice.js
@@ -18,23 +18,28 @@ export default function SexChoice({ setSexe, initialNames, setName, setId, setNe
   };
 
   return (
-    <div className="d-flex justify-content-center">
-      <button
-        type="button"
-        className="btn btn-outline-success btn-lg mr-2  custom"
-        onClick={handleChange}
-        value="m"
-      >
-        Garçon
-      </button>
-      <button
-        type="button"
-        className="btn btn-outline-success btn-lg mr-2  custom"
-        onClick={handleChange}
-        value="f"
-      >
-        Fille
-      </button>
-    </div>
+    <>
+      <p className="slogan mt-4 mb-5 px-2">
+        « Découvrez les tendances des utilisateurs qui ont les mêmes préférences que vous »
+      </p>
+      <div className="d-flex justify-content-center">
+        <button
+          type="button"
+          className="btn btn-outline-success btn-lg mr-2  custom"
+          onClick={handleChange}
+          value="m"
+        >
+          Garçon
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-success btn-lg mr-2  custom"
+          onClick={handleChange}
+          value="f"
+        >
+          Fille
+        </button>
+      </div>
+    </>
   );
 }
